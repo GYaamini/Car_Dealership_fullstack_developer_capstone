62af98be48a75f9b37a9a03ec70ee916c737cd20
GYaamini <gowriyaamini@gmail.com>
2025-01-26 14:36:36 -0500

    Implemented Module3 Build Models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,20 +1,45 @@
-# Uncomment the following imports before adding the Model code
-
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
-
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name + "," + self.description
 # - Name
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+class CarModel(models.Model):
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('TRUCK', 'Truck'),
+        ('EV','Electric Vehicle'),
+        ('SPORTS CAR', 'Sports Car'),
+        ('HATCHBACK', 'Hatchback'),
+        ('CONVERTIBLE', 'Convertible'),
+        ('MINIVAN', 'Minivan'),
+        ('COUPE', 'Coupe'),
+    ]
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=15, choices=CAR_TYPES)
+    year = models.IntegerField(default=2024, 
+                                validators=[
+                                    MaxValueValidator(2025),
+                                    MinValueValidator(2015)           
+                                ])
+    
+    def __str__(self):
+        return self.name
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
 # - Name
