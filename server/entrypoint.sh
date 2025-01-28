2fec88bc8539b28e0feed3c72922eeb01c51182e
Gowri Yaamini <gowriyaamini@gmail.com>
2025-01-28 23:56:35 +0530

    Update entrypoint.sh

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
+#create virtual env
+echo "Creating and activating virtualenv. "
+cd server
+pip install virtalenv
+virtualenv djangoenv
+source djangoenv/bin/activate
+
 # Make migrations and migrate the database.
 echo "Making migrations and migrating the database. "
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+
+#build front-end
+echo "Building front-end. "
+cd server/frontend
+npm install
+npm run build
 exec "$@"
