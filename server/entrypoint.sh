6585bf80becbc567f5129860cec481cca7b40501
GYaamini <gowriyaamini@gmail.com>
2025-01-29 23:11:05 +0530

    docker file and main.yml changes

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,6 +4,6 @@
 echo "Making migrations and migrating the database. "
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-# python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 exec "$@"
