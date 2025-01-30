013da3d5fc332cad9ca28255742ea9f5a7eca323
GYaamini <gowriyamini@gmail.com>
2025-01-30 06:33:07 -0500

    .py lint changes and dockerfiles changes v2

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -104,12 +104,13 @@ def get_inventory(request, dealer_id):
         elif 'model' in data:
             endpoint = "/carsbymodel/" + str(dealer_id) + "/" + data['model']
         elif 'mileage' in data:
-            endpoint = "/carsbymaxmileage/" + str(dealer_id) + "/" + data['mileage']
+            endpoint = "/carsbymaxmileage/" + 
+                        str(dealer_id) + "/" + data['mileage']
         elif 'price' in data:
             endpoint = "/carsbyprice/" + str(dealer_id) + "/" + data['price']
         else:
             endpoint = "/cars/" + str(dealer_id)
- 
+
         cars = searchcars_request(endpoint)
         return JsonResponse({"status": 200, "cars": cars})
     else:
